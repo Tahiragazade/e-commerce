2edd32ccc3cc5b71ebf152024df8061668aeadb9
Tahir Aghazada <88485652+Tahiragazade@users.noreply.github.com>
2023-01-24 15:24:29 +0400

    updated orders in adminPanel

---
+++ backend/views/order/_order-products.php
@@ -0,0 +1,71 @@
+<?php
+
+use common\models\OrderProduct;
+use yii\helpers\Html;
+use yii\helpers\Url;
+use yii\grid\ActionColumn;
+use yii\grid\GridView;
+//use kartik\grid\GridView;
+/** @var yii\web\View $this */
+/** @var backend\models\OrderProductSearch $searchModel */
+/** @var yii\data\ActiveDataProvider $dataProvider */
+
+$this->title = Yii::t('app', 'Order Products');
+$this->params['breadcrumbs'][] = $this->title;
+?>
+<div class="order-product-index">
+
+
+    <?php // echo $this->render('_search', ['model' => $searchModel]); ?>
+
+    <?= GridView::widget([
+        'dataProvider' => $dataProvider,
+        'filterModel' => $searchModel,
+        'columns' => [
+            ['class' => 'yii\grid\SerialColumn'],
+
+            'id',
+            'order_id',
+	        [
+		        'attribute' => 'photo',
+		        'format' => 'html',
+		        'value'=>function($data) {  return Html::img($data->product->small_photo,
+			        ['width' => '70px']); },
+	        ],
+	        [
+		        'attribute' => 'product_id',
+		        'value' => function($data) {
+			        return $data->product->title;
+		        }
+	        ],
+	        [
+		        'attribute' => 'size_id',
+		        'value' => function($data) {
+			        return $data->size->name;
+		        }
+	        ],
+	        [
+		        'attribute' => 'color_id',
+		        'value' => function($data) {
+			        return $data->color->name;
+		        }
+	        ],
+	        [
+		        'attribute' => 'link',
+		        'format' => 'html',
+		        'value'=>function($data) {  return Url::to('https://'.Yii::$app->params['frontendBaseUrl'].'/site/details/'.$data->product_id.'',true);},
+	        ],
+            'discounted_price',
+            'count',
+            'price',
+            [
+                'class' => ActionColumn::className(),
+                'urlCreator' => function ($action, OrderProduct $model, $key, $index, $column) {
+                    return Url::toRoute([$action, 'id' => $model->id]);
+                 }
+            ],
+        ],
+    ]); ?>
+
+
+</div>
--- backend/views/order/index.php
+++ backend/views/order/index.php
@@ -1,50 +1,72 @@
 <?php
 
+use backend\models\OrderProductSearch;
+use backend\models\OrderSearch;
 use common\models\Order;
 use yii\helpers\Html;
 use yii\helpers\Url;
 use yii\grid\ActionColumn;
-use yii\grid\GridView;
+
+//use yii\grid\GridView;
+use kartik\grid\GridView;
 
 /** @var yii\web\View $this */
 /** @var backend\models\OrderSearch $searchModel */
 /** @var yii\data\ActiveDataProvider $dataProvider */
 
 $this->title = Yii::t('app', 'Orders');
-$this->params['breadcrumbs'][] = $this->title;
+//$this->params['breadcrumbs'][] = $this->title;
 ?>
 <div class="order-index">
 
     <h1><?= Html::encode($this->title) ?></h1>
 
     <p>
-        <?= Html::a(Yii::t('app', 'Create Order'), ['create'], ['class' => 'btn btn-success']) ?>
+		<?= Html::a(Yii::t('app', 'Create Order'), ['create'], ['class' => 'btn btn-success']) ?>
     </p>
 
-    <?php // echo $this->render('_search', ['model' => $searchModel]); ?>
+	<?php // echo $this->render('_search', ['model' => $searchModel]); ?>
 
-    <?= GridView::widget([
-        'dataProvider' => $dataProvider,
-        'filterModel' => $searchModel,
-        'columns' => [
-            ['class' => 'yii\grid\SerialColumn'],
-
-            'id',
-            'user_id',
-            'session_id',
-            'created_at',
-            'updated_at',
-            //'payment_type',
-            //'total_price',
-            //'shipping_price',
-            [
-                'class' => ActionColumn::className(),
-                'urlCreator' => function ($action, Order $model, $key, $index, $column) {
-                    return Url::toRoute([$action, 'id' => $model->id]);
-                 }
-            ],
-        ],
-    ]); ?>
+	<?= GridView::widget([
+		'dataProvider' => $dataProvider,
+		'filterModel' => $searchModel,
+		'columns' => [
+                [
+			'class' => 'kartik\grid\ExpandRowColumn',
+			'value' => function($model, $key, $index, $column) {
+				return GridView::ROW_COLLAPSED;
+			},
+			'detail' => function($model, $key, $index, $column) {
+				$searchModel = new OrderProductSearch();
+				$searchModel->order_id = $model->id;
+				$dataProvider = $searchModel->search(Yii::$app->request->queryParams);
+				return Yii::$app->controller->renderPartial('_order-products', [
+					'searchModel' => $searchModel,
+					'dataProvider' => $dataProvider
+				]);
+			},
+],
+			'id',
+			'user_id',
+			'session_id',
+			[
+				'attribute' => 'created_at',
+				'value' => function($data) {
+					return date('Y-m-d',$data->created_at);
+				}
+			],
+//			'updated_at',
+			'payment_type',
+			'total_price',
+			'shipping_price',
+//			[
+//				'class' => ActionColumn::className(),
+//				'urlCreator' => function($action, Order $model, $key, $index, $column) {
+//					return Url::toRoute([$action, 'id' => $model->id]);
+//				}
+//			],
+		],
+	]); ?>
 
 
 </div>
